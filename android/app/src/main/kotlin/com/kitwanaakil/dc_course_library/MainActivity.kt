a32621ac630c28ab1db40a27407a480bae651604
Kitwana Akil <kakil@me.com>
2023-05-14 17:11:01 -0400

    Initial commit

---
+++ android/app/src/main/kotlin/com/kitwanaakil/dc_course_library/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.kitwanaakil.dc_course_library
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Row(
+            children: [
+              Container(
+                width: 42.0,
+                height: 42.0,
+                padding: const EdgeInsets.all(10.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(14.0),
+                  gradient: const LinearGradient(
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                      colors: [
+                        Color(0xFF00AEFF),
+                        Color(0xFF0076FF),
+                      ]),
+                ),
+                child: const Icon(
+                  Icons.home,
+                  color: Colors.white,
+                ),
+              ),
+              const SizedBox(
+                width: 12,
+              ),
+              Container(
+                child: const Text(
+                  "Home",
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.w800,
+                    color: Color(0xFF242629),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
